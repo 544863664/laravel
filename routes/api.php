8a26bdad388a4ff45c4732b3f7b68ff30ee37726
liguodong <1953674840@qq.com>
2020-09-27 11:17:00 +0800

    two commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::any('/bar', function () {
+    return 'This is a request from any HTTP verb';
+});
