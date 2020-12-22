7c29d88f167cb04b7ea466e6648ac53453b494b2
李国栋 <1953674840@qq.com>
2020-12-22 12:56:40 +0800

    2

--- app/Http/Controllers/Api/Jscode2session.php
+++ app/Http/Controllers/Api/Jscode2session.php
@@ -9,18 +9,30 @@ class Jscode2session extends Controller
 {
     //
 	public function getJscode2session(Request $request) {
-		$response = array("code" => 200, "msg" => "success");
+		$response = array("code" => 200, "msg" => "success", "data" => "");
 		
-		$code = $request->code;
 		$appid = 'wx0ac5669cb2a4a6b8';
 		$secret = '803bf67e6eed2786824e4db88772f87a';
 		
-		$url = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid='.$appid.'&secret='.$secret.'&js_code='.$code;
+		$code = $request->code;
 		
-		$jscode2session = file_get_contents($url);
-		
-		$response['session_key'] = json_decode($jscode2session, true)['session_key'];
-		$response['openid'] = json_decode($jscode2session, true)['openid'];
+		if ($code) {
+			// $jscode2sessionurl = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid='.$appid.'&secret='.$secret.'&js_code='.$code;
+			
+			// $jscode2session = file_get_contents($jscode2sessionurl);
+			
+			// $response['data'] = json_decode($jscode2session, true);
+			
+			$access_tokenurl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$secret;
+			
+			$access_token = file_get_contents($access_tokenurl);
+			
+			$response['data'] = json_decode($access_token, true);
+			// var_dump($request->header('token'));
+			return $response;
+		}
+		$response['code'] = 0;
+		$response['msg'] = 'The parameter code cannot be null.';
 		return $response;
 	}
 }
