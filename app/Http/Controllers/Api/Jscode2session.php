8896973ff6ae16f3f4179407e2a3a0e351ebe31f
李国栋 <1953674840@qq.com>
2020-12-22 17:08:36 +0800

    gettoken wechat

--- app/Http/Controllers/Api/Jscode2session.php
+++ app/Http/Controllers/Api/Jscode2session.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Jscode2session extends Controller
 {
@@ -15,21 +16,49 @@ class Jscode2session extends Controller
 		$secret = '803bf67e6eed2786824e4db88772f87a';
 		
 		$code = $request->code;
+		# 获取header传来的token var_dump($request->header('token'));
 		
 		if ($code) {
-			// $jscode2sessionurl = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid='.$appid.'&secret='.$secret.'&js_code='.$code;
+			# 获取用户openid
+			$jscode2sessionurl = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid='.$appid.'&secret='.$secret.'&js_code='.$code;
 			
-			// $jscode2session = file_get_contents($jscode2sessionurl);
+			$jscode2session = file_get_contents($jscode2sessionurl);
 			
-			// $response['data'] = json_decode($jscode2session, true);
-			
+			# 获取用户token
 			$access_tokenurl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$secret;
 			
 			$access_token = file_get_contents($access_tokenurl);
 			
 			$response['data'] = json_decode($access_token, true);
-			// var_dump($request->header('token'));
-			return $response;
+			
+			$openid = json_decode($jscode2session, true)['openid'];
+			$session_key = json_decode($jscode2session, true)['session_key'];
+			
+			$setOpenid = DB::select( 'select * from wechatuser where openid = ?', [$openid] );
+			
+			if (!$setOpenid) {
+				# 新用户创建数据
+				$openidInsert = DB::insert(
+					'insert into wechatuser (id, openid, session_key, nickName, gender, language, country, city, province, avatarUrl, status) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+					[NULL, $openid, $session_key, '', '', '', '', '', '', '', '1']
+				);
+				return $response;
+			} 
+			// else {
+			// 	# 老用户读取、修改数据
+			// 	$id_ = '';
+			// 	foreach($setOpenid as $row) { $id_ = $row->id; }
+				
+			// 	$openidUpdate = DB::update(
+			// 		'update wechatuser set nickName=?, gender=?, language=?, country=?, city=?, province=?, avatarUrl=?, status=? where id= ?',
+			// 		['fdfaeasdfa', '1', '', '', '', '', '', '', $id_]
+			// 	);
+			// 	if ($openidUpdate > 0) {
+			// 		return $response;
+			// 	}
+			// }
+			
+			
 		}
 		$response['code'] = 0;
 		$response['msg'] = 'The parameter code cannot be null.';
